b43dfe15de5197dfaa108273193c385ff63e37be
Maksym Kravchuk <kravchukmaxym@gmail.com>
2020-04-10 20:26:09 +0300

    Created 'header', 'nav', 'menu', 'search' and 'card' blocks.

--- js/main.js
+++ js/main.js
@@ -1,10 +1,43 @@
 $(document).ready(function() {
 
     // MENU
-    if ($('.menu__btn').length) {
-        
+    if ($('#menu').length) {
+
+        const menu    = $('#menu');
+        const menuBtn = $('.menu__btn');
+
         $(document).click(function(e) {
-            $(e.target).closest('.menu__btn').toggleClass('menu__btn--open')
+
+            if ($(e.target).closest('.menu__btn').length || $(e.target).hasClass('menu')) {
+                menuBtn.toggleClass('menu__btn--open')
+                menu.toggleClass('menu--open')
+            }
+        })
+    }
+
+    // SEARCH
+    if ($('#search').length) {
+
+        const search = $('#search');
+
+        $(document).click(function(e) {
+
+            if ($(e.target).closest('.search__btn').length || $(e.target).hasClass('search')) {
+                search.toggleClass('search--open')
+            }
+        })
+    }
+
+    // CARD
+    if ($('#card').length) {
+
+        const card = $('#card');
+
+        $(document).click(function(e) {
+
+            if ($(e.target).closest('.card__btn').length || $(e.target).hasClass('card')) {
+                card.toggleClass('card--open')
+            }
         })
     }
 })
